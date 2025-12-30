f9045245fd2309737d86b3f6acc10366fd3d94ac
gfournierPro <gabriel.fournier.10@gmail.com>
2025-12-30 23:33:40 +0100

    chore: update dependencies and configuration for smart contract deployment
    - Replaced hardhat-ignition and hardhat-toolbox-viem with hardhat-ethers.
    - Downgraded hardhat to version 2.22.0 for compatibility.
    - Updated hardhat-verify to version 2.0.0.
    - Added @openzeppelin/contracts-upgradeable as a dependency.
    - Modified TypeScript configuration to target ES2020 and use commonjs module system.
    - Enabled declaration generation and added additional compiler options for better consistency.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,45 +1,24 @@
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable, defineConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-verify";
 import "@openzeppelin/hardhat-upgrades";
 
-
-export default defineConfig({
-  plugins: [hardhatToolboxViemPlugin],
+const config: HardhatUserConfig = {
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
   },
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
     },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-    },
-    arbitrum:{
-      
-    } 
   },
-});
+};
+
+export default config;
